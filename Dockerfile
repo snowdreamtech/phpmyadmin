d8fe656405ce4b608978413b918fbf6f41ea918e
snowdream <sn0wdr1am@qq.com>
2024-08-04 00:02:03 +0800

    feat: ENV PMA_ARBITRARY=0 \     PMA_AUTH_TYPE="cookie" \     PMA_HOST="localhost" \     PMA_VERBOSE="localhost" \     PMA_PORT=3306 \     PMA_USER="root" \     PMA_PASS=""

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,16 @@ FROM snowdreamtech/php:8.1.29
 
 LABEL maintainer="snowdream <sn0wdr1am@qq.com>"
 
-ENV PMA_ARBITRARY=0
+ENV PMA_ARBITRARY=0 \
+    PMA_AUTH_TYPE="cookie" \
+    PMA_HOST="localhost" \
+    PMA_VERBOSE="localhost" \
+    PMA_PORT=3306 \
+    PMA_USER="root" \
+    PMA_PASS="" 
 
 RUN apk add --no-cache --repository=https://dl-cdn.alpinelinux.org/alpine/v3.17/community phpmyadmin=5.2.1-r0
-    
+
 COPY docker-entrypoint.sh /usr/local/bin/
 
 ENTRYPOINT ["docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,10 +6,40 @@ PHPMYADMIN_CONFIG_PATH=/usr/share/webapps/phpmyadmin/config.inc.php
 # Modifying configuration file config.inc.php
 # https://github.com/phpmyadmin/docker/blob/master/config.inc.php
 if [ "$PMA_ARBITRARY" -eq 1 ]; then
-    sed -i "/\$cfg['AllowArbitraryServer']\s*=\s*.+/d" ${PHP_CONFIG_PATH}
-    echo "\$cfg['AllowArbitraryServer'] = true;" >>${PHP_CONFIG_PATH}
+    sed -i "/\$cfg['AllowArbitraryServer']\s*=\s*.+/d" ${PHPMYADMIN_CONFIG_PATH}
+    echo "\$cfg['AllowArbitraryServer'] = true;" >>${PHPMYADMIN_CONFIG_PATH}
 else
-    sed -i "/\$cfg['AllowArbitraryServer']\s*=\s*.+/d" ${PHP_CONFIG_PATH}
+    sed -i "/\$cfg['AllowArbitraryServer']\s*=\s*.+/d" ${PHPMYADMIN_CONFIG_PATH}
+fi
+
+if [ -n "${PMA_AUTH_TYPE}" ]; then
+    sed -i "/\$cfg\['Servers'\]\[\$i\]\['auth_type'\]/d" ${PHPMYADMIN_CONFIG_PATH}
+    echo "\$cfg['Servers'][\$i]['auth_type'] = '${PMA_AUTH_TYPE}';" >>${PHPMYADMIN_CONFIG_PATH}
+fi
+
+if [ -n "${PMA_HOST}" ]; then
+    sed -i "/\$cfg\['Servers'\]\[\$i\]\['host'\]/d" ${PHPMYADMIN_CONFIG_PATH}
+    echo "\$cfg['Servers'][\$i]['host'] = '${PMA_HOST}';" >>${PHPMYADMIN_CONFIG_PATH}
+fi
+
+if [ -n "${PMA_VERBOSE}" ]; then
+    sed -i "/\$cfg\['Servers'\]\[\$i\]\['verbose'\]/d" ${PHPMYADMIN_CONFIG_PATH}
+    echo "\$cfg['Servers'][\$i]['verbose'] = '${PMA_VERBOSE}';" >>${PHPMYADMIN_CONFIG_PATH}
+fi
+
+if [ -n "${PMA_PORT}" ]; then
+    sed -i "/\$cfg\['Servers'\]\[\$i\]\['port'\]/d" ${PHPMYADMIN_CONFIG_PATH}
+    echo "\$cfg['Servers'][\$i]['port'] = ${PMA_PORT};" >>${PHPMYADMIN_CONFIG_PATH}
+fi
+
+if [ -n "${PMA_USER}" ]; then
+    sed -i "/\$cfg\['Servers'\]\[\$i\]\['user'\]/d" ${PHPMYADMIN_CONFIG_PATH}
+    echo "\$cfg['Servers'][\$i]['user'] = '${PMA_USER}';" >>${PHPMYADMIN_CONFIG_PATH}
+fi
+
+if [ -n "${PMA_PASS}" ]; then
+    sed -i "/\$cfg\['Servers'\]\[\$i\]\['password'\]/d" ${PHPMYADMIN_CONFIG_PATH}
+    echo "\$cfg['Servers'][\$i]['password'] = '${PMA_PASS}';" >>${PHPMYADMIN_CONFIG_PATH}
 fi
 
 # exec commands
