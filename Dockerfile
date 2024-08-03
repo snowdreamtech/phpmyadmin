04d764271f4a942f8a31b43f2ffa47d0967cad8c
snowdream <sn0wdr1am@qq.com>
2024-08-03 20:29:56 +0800

    feat: PMA_ARBITRARY

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,9 @@ FROM snowdreamtech/php:8.1.29
 
 LABEL maintainer="snowdream <sn0wdr1am@qq.com>"
 
-RUN apk add --no-cache phpmyadmin=5.2.1-r2
+ENV PMA_ARBITRARY=0
+
+RUN apk add --no-cache --repository=https://dl-cdn.alpinelinux.org/alpine/v3.17/community phpmyadmin=5.2.1-r0
     
 COPY docker-entrypoint.sh /usr/local/bin/
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 set -e
 
+PHPMYADMIN_CONFIG_PATH=/usr/share/webapps/phpmyadmin/config.inc.php
+
+# Modifying configuration file config.inc.php
+# https://github.com/phpmyadmin/docker/blob/master/config.inc.php
+if [ "$PMA_ARBITRARY" -eq 1 ]; then
+    sed -i "/\$cfg['AllowArbitraryServer']\s*=\s*.+/d" ${PHP_CONFIG_PATH}
+    echo "\$cfg['AllowArbitraryServer'] = true;" >>${PHP_CONFIG_PATH}
+else
+    sed -i "/\$cfg['AllowArbitraryServer']\s*=\s*.+/d" ${PHP_CONFIG_PATH}
+fi
+
 # exec commands
 exec "$@"
